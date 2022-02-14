144f5cb21da2cb1b0e057020990666768e1de4f5
Daojun Li <2678903l@student.gla.ac.uk>
2022-02-14 15:53:42 +0000

    Chapter 6 exercises completed

--- populate_rango.py
+++ populate_rango.py
@@ -18,32 +18,32 @@ def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
         'url':'http://docs.python.org/3/tutorial/',
-        'views':30},
+        'views':114},
         {'title':'How to Think like a Computer Scientist',
         'url':'http://www.greenteapress.com/thinkpython/',
-        'views':15},
+        'views':53},
         {'title':'Learn Python in 10 Minutes',
         'url':'http://www.korokithakis.net/tutorials/python/',
-        'views':60} ]
+        'views':20} ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
-        'views':22},
+        'views':32},
         {'title':'Django Rocks',
         'url':'http://www.djangorocks.com/',
-        'views':17},
+        'views':12},
         {'title':'How to Tango with Django',
         'url':'http://www.tangowithdjango.com/',
-        'views':36} ]
+        'views':1258} ]
 
     other_pages = [
         {'title':'Bottle',
         'url':'http://bottlepy.org/docs/dev/',
-        'views':13},
+        'views':54},
         {'title':'Flask',
         'url':'http://flask.pocoo.org',
-        'views':7} ]
+        'views':64} ]
 
     cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
             'Django': {'pages': django_pages,'views':64,'likes':32},
@@ -59,14 +59,14 @@ def populate():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
-def add_page(cat, title, url, views):
-    p = Page.objects.get_or_create(category=cat, title=title,views=views)[0]
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
     p.save()
     return p
 
-def add_cat(name,views,likes):
+def add_cat(name,views=0,likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views=views
     c.likes=likes
